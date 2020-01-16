50c9cea7064c7e310ae67f71461044db13da2b82
my0419 <my0419@protonmail.com>
2020-01-16 21:02:27 +0700

    update wireguard gist

--- installer/type.go
+++ installer/type.go
@@ -19,7 +19,7 @@ func listOfTypes() map[string]string {
 		TypeL2TP:        "https://gist.githubusercontent.com/my0419/560071251f2427a9a19862d8a04edb94/raw/4357bafd1e25192b42818cac785d205b4e0bd84b/l2tp.sh",
 		TypePPTP:        "https://gist.githubusercontent.com/my0419/db77a7bdb466b9df01ffa3f96f4b3f37/raw/80d309fc7a08817a94d4d1ffc15eddf2bca4d29f/pptp.sh",
 		TypeOpenVPN:     "https://gist.githubusercontent.com/my0419/73ba68f383b5772030078ec871456b06/raw/6e87ac14b3ce94ee94938dcfac9eb80f8d974ef5/openvpn.sh",
-		TypeWireGuard:   "https://gist.githubusercontent.com/my0419/dd0111d60375dc756c19a70e0907e32b/raw/f40f94435d75efefb9bbe5fa5fbc1d7783484387/wireguard.sh",
+		TypeWireGuard:   "https://gist.githubusercontent.com/my0419/dd0111d60375dc756c19a70e0907e32b/raw/05ca85bf110199ab35421e7702bf6bc184323603/wireguard.sh",
 		TypeTestSuccess: "https://gist.githubusercontent.com/my0419/4b5eeaaa98f4b5ae7eeee7b2f5b5cb9a/raw/e730a11c7d5d2caf9ea63ee672f9a195250d06be/test-success.sh",
 		TypeTestFail:    "https://gist.githubusercontent.com/my0419/b4868d16abdd5e43ee7b58c71b079529/raw/a71ee1dbba6d066282db85587ecbf757f4703281/test-fail.sh",
 	}
