321d22a5c6ad8af8dcd345e74253426143eae90b
my0419 <my0419@protonmail.com>
2020-12-26 14:01:11 +0700

    wireguard / shadowsocks multi accounts

--- installer/type.go
+++ installer/type.go
@@ -21,10 +21,10 @@ func listOfTypes() map[string]string {
 		TypeL2TP:        "https://gist.githubusercontent.com/my0419/560071251f2427a9a19862d8a04edb94/raw/cbccae8b44578ca330192843f1b828f16b5e013d/l2tp.sh",
 		TypePPTP:        "https://gist.githubusercontent.com/my0419/db77a7bdb466b9df01ffa3f96f4b3f37/raw/2889061402fbe1dd247316caed0ca45722ca9b07/pptp.sh",
 		TypeOpenVPN:     "https://gist.githubusercontent.com/my0419/73ba68f383b5772030078ec871456b06/raw/c20873b922cb06224d3a9b2be9a38676dec4365a/openvpn.sh",
-		TypeWireGuard:   "https://gist.githubusercontent.com/my0419/dd0111d60375dc756c19a70e0907e32b/raw/2f6495d24484611a69df5fe416d1b9c862bc2a22/wireguard.sh",
+		TypeWireGuard:   "https://gist.githubusercontent.com/my0419/dd0111d60375dc756c19a70e0907e32b/raw/168aa42b994625d4160602d52787d068ee84b9bd/wireguard.sh",
 		TypeTestSuccess: "https://gist.githubusercontent.com/my0419/4b5eeaaa98f4b5ae7eeee7b2f5b5cb9a/raw/e730a11c7d5d2caf9ea63ee672f9a195250d06be/test-success.sh",
 		TypeTestFail:    "https://gist.githubusercontent.com/my0419/b4868d16abdd5e43ee7b58c71b079529/raw/a71ee1dbba6d066282db85587ecbf757f4703281/test-fail.sh",
-		TypeShadowsocks: "https://gist.githubusercontent.com/my0419/44f0e8aa1b70fa887432ad6d1d376830/raw/1f5a68ea3c28d15276719935c22bfcf150bdfff9/shadowsocks.sh",
+		TypeShadowsocks: "https://gist.githubusercontent.com/my0419/44f0e8aa1b70fa887432ad6d1d376830/raw/cdd05882e4e24bd2b4979b743a7f68ccfe8bb37f/shadowsocks.sh",
 		TypeSocksFive:   "https://gist.githubusercontent.com/my0419/5ddf74cb80eed50d19ae799d37fcaecc/raw/089c76f4f58f73862997e821f59e255636904fa7/socks5.sh",
 	}
 }
